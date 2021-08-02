6ea6170c4de7cdfb487ac4f299a367da513ffc6a
kang1seok <kang1seok@gmail.com>
2021-08-02 10:35:41 +0900

    Second commit

---
+++ opencv_webapp/admin.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from .models import ImageUploadModel
+
+class image_upload_Admin(admin.ModelAdmin):
+
+    list_display = ('description', 'document', ) # list_display 변수명은 고정
+
+admin.site.register(ImageUploadModel, image_upload_Admin)
